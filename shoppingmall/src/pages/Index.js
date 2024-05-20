70551a02f794e5d86607a2f2243dac9747c65b6f
ujeans <dbwlsxkal95@gmail.com>
2024-05-20 21:51:54 +0900

    setting: 기본 폴더 구조 세팅

---
+++ shoppingmall/src/pages/Index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Components from "../components/Components";
+
+const Index = () => {
+  return (
+    <div>
+      <Components />
+    </div>
+  );
+};
+
+export default Index;
