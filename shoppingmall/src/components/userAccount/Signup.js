1bc2ed25e39353978714a0cfe59d214991b3f9b8
kywu9232 <107646059+kywu9232@users.noreply.github.com>
2024-05-23 02:13:45 +0900

    feature : 회원가입 로직 구현
    회원가입 로직 1차 구현 추후 수정 필요

--- shoppingmall/src/components/userAccount/Signup.js
+++ shoppingmall/src/components/userAccount/Signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { theme } from "../../style/theme";
 
@@ -7,8 +7,76 @@ import email from "../../assets/email.svg";
 import password from "../../assets/password.svg";
 import people from "../../assets/people.svg";
 import phone from "../../assets/phone.svg";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  //유저 정보
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+    passwordCheck: "",
+    name: "",
+    phone: "",
+    addr: "",
+    nickname: "",
+    img: null,
+  });
+
+  //이미지 미리보기
+  // const [preview, setPreview] = useState(null);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserInfo((userInfo) => ({
+      ...userInfo,
+      [name]: value,
+    }));
+  };
+
+  //isVaild
+  const isVaild =
+    userInfo.email &&
+    userInfo.name &&
+    userInfo.phone &&
+    userInfo.addr &&
+    userInfo.password.length >= 8 &&
+    userInfo.password.length <= 20 &&
+    userInfo.password === userInfo.passwordCheck;
+
+  //회원가입
+  const submitSignup = async () => {
+    console.log(userInfo);
+    try {
+      await fetch("/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({
+          email: userInfo.email,
+          password: userInfo.password,
+          name: userInfo.name,
+          phone: userInfo.phone,
+          addr: userInfo.addr,
+          nickname: userInfo.nickname,
+        }),
+      });
+
+      // if (!response.ok) {
+      //   throw new Error("회원가입에 실패하였습니다.");
+      // }
+      moveToLogin();
+    } catch (error) {
+      console.error("에러", error);
+    }
+  };
+
+  const navigate = useNavigate();
+  const moveToLogin = () => {
+    navigate("/login", userInfo);
+    console.log(userInfo);
+  };
+
   return (
     <>
       <Wrapper>
@@ -21,42 +89,89 @@ const Signup = () => {
             <IconWrapper>
               <Icon src={email} />
             </IconWrapper>
-            <Input placeholder="아이디(이메일)" type="email" required />
+            <Input
+              placeholder="아이디(이메일)"
+              type="email"
+              value={userInfo.email}
+              name="email"
+              onChange={handleInputChange}
+            />
           </InputContainer>
           <InputContainer>
             <IconWrapper>
               <Icon src={password} />
             </IconWrapper>
-            <Input placeholder="비밀번호" type="password" required />
+            <Input
+              placeholder="비밀번호"
+              type="password"
+              value={userInfo.password}
+              name="password"
+              onChange={handleInputChange}
+            />
           </InputContainer>
           <InputContainer>
             <IconWrapper>
               <Icon src={password} />
             </IconWrapper>
-            <Input placeholder="비밀번호 확인" type="password" required />
+            <Input
+              placeholder="비밀번호 확인"
+              type="password"
+              value={userInfo.passwordCheck}
+              name="passwordCheck"
+              onChange={handleInputChange}
+            />
           </InputContainer>
           <InputContainer>
             <IconWrapper>
               <Icon src={people} />
             </IconWrapper>
-            <Input placeholder="이름" required />
+            <Input
+              placeholder="이름"
+              value={userInfo.name}
+              name="name"
+              onChange={handleInputChange}
+            />
           </InputContainer>
           <InputContainer>
             <IconWrapper>
               <Icon src={phone} />
             </IconWrapper>
-            <Input placeholder="휴대폰 번호" required />
+            <Input
+              type="number"
+              placeholder="휴대폰 번호"
+              value={userInfo.phone}
+              name="phone"
+              onChange={handleInputChange}
+            />
+          </InputContainer>
+          <InputContainer>
+            <IconWrapper>
+              <Icon src={phone} />
+            </IconWrapper>
+            <Input
+              placeholder="주소"
+              value={userInfo.addr}
+              name="addr"
+              onChange={handleInputChange}
+            />
           </InputContainer>
 
           <FileInputWrapper>
             <FileInfo>나를 나타내는 프로필 사진을 등록해주세요</FileInfo>
-            <Input type="file" accept="image/*" />
+            <Input type="file" accept="image/*" name="img" />
           </FileInputWrapper>
           <InputContainer>
-            <Input placeholder="닉네임" required />
+            <Input
+              placeholder="닉네임"
+              value={userInfo.nickname}
+              name="nickname"
+              onChange={handleInputChange}
+            />
           </InputContainer>
-          <SubmitButton>가입하기</SubmitButton>
         </InputWrapper>
+        <SubmitButton disabled={!isVaild} onClick={submitSignup}>
+          가입하기
+        </SubmitButton>
       </Wrapper>
     </>
   );
@@ -147,9 +262,13 @@ const SubmitButton = styled.button`
   max-width: 465px;
   height: 45px;
   border-radius: 10px;
-  border: 0px;
+  border: none;
   background-color: ${theme.mainColor};
   color: #ffffff;
   font-weight: 700;
   cursor: pointer;
+  &:disabled {
+    background-color: "#F4F4F4";
+    cursor: not-allowed;
+  }
 `;
