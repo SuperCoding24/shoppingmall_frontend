e1055eb49ec4e79997897eb4231aa71d37a8eb18
choimeeyoung <choimeeyoung94@gmail.com>
2024-05-29 12:40:29 +0900

    ItemFilter, ItemListPage 파일명 변경

--- shoppingmall/src/components/itemList/ItemFilter.js
+++ shoppingmall/src/components/itemList/ItemFilter.js
@@ -7,8 +7,9 @@ import styled from "styled-components";
 import pencil from "../../assets/pencil.svg";
 import categorydropdown from "../../assets/categorydropdown.svg";
 import Modal from "../commom/Modal/Modal";
+import { BlackBtn } from "../../style/CommonStyles";
 
-const ProductFilter = () => {
+const ItemFilter = () => {
   const navigate = useNavigate();
   const [btnActive, setBtnActive] = useState(0);
   const [isVisible, setIsVisible] = useState(false);
@@ -41,77 +42,80 @@ const ProductFilter = () => {
   const handleClickButton = (idx) => {
     setBtnActive(idx);
   };
-
   return (
-    <ProductListFilterWrapper>
-      <ButtonWrapper>
-        <ButtonDivs>
-          <Menu onClick={() => handleClickButton(0)} isActive={btnActive === 0}>
-            추천순
-          </Menu>
-          <Menu onClick={() => handleClickButton(1)} isActive={btnActive === 1}>
-            가격 낮은순
-          </Menu>
-          <Menu
-            onClick={() => handleClickButton(2)}
-            isActive={btnActive === 2}
-            isH
-          >
-            가격 높은순
-          </Menu>
-          <CategoryButton
-            onCLick={() => {
-              setDropdownToggled(true);
-            }}
-          >
-            카테고리
-            <CategoryIcon src={categorydropdown} />
-          </CategoryButton>
-          <Options active={isDropdownToggled}>
-            {selectList.map((option, index) => {
-              return <button>{option.name}</button>;
-            })}
-          </Options>
-          <WriteButtonWrapper>
-            <ItemAddButton onClick={checkLogin}>상품 등록</ItemAddButton>
-            <Icon src={pencil} />
-          </WriteButtonWrapper>
-        </ButtonDivs>
-      </ButtonWrapper>
-      {isVisible && (
-        <Modal
-              open={isVisible} 
-              onClose={closeModal} 
-              title="로그인이 필요한 기능입니다." 
-              subText="로그인 페이지로 이동하시겠습니까?" 
-              navigateToPage={navigateToPage}/>
-      )}
-    </ProductListFilterWrapper>
+      <FilterContainer>
+          <LeftButtons>
+              <Menu onClick={() => handleClickButton(0)} isActive={btnActive === 0}>
+                  추천순
+                </Menu>
+                <Menu onClick={() => handleClickButton(1)} isActive={btnActive === 1}>
+                  가격 낮은순
+                </Menu>
+                <Menu
+                  onClick={() => handleClickButton(2)}
+                  isActive={btnActive === 2}
+                  isH
+                >
+                  가격 높은순
+                </Menu>
+                <CategoryButton
+                  onCLick={() => {
+                    setDropdownToggled(true);
+                  }}
+                >
+                  카테고리
+                  <CategoryIcon src={categorydropdown} />
+                </CategoryButton>
+                <Options active={isDropdownToggled}>
+                  {selectList.map((option, index) => {
+                    return <button>{option.name}</button>;
+                  })}
+                </Options>
+          </LeftButtons>
+          <RightButtons>
+                <ItemAddButton onClick={checkLogin}>상품 등록</ItemAddButton>
+                <Icon src={pencil} />
+          </RightButtons>
+          {isVisible && (
+          <Modal
+                open={isVisible} 
+                onClose={closeModal} 
+                title="로그인이 필요한 기능입니다." 
+                subText="로그인 페이지로 이동하시겠습니까?" 
+                navigateToPage={navigateToPage}/>
+        )}
+      </FilterContainer>
   );
 };
 
-export default ProductFilter;
 
-const ProductListFilterWrapper = styled.div`
-  display: flex;
-  margin-top: 42px;
-`;
+export default ItemFilter;
 
-const ButtonWrapper = styled.div`
+const FilterContainer= styled.div`
     display: flex;
-    width: 1000px
-    height: 50px;
-    margin-left: 20px;
-    justify-content: flex-start;
+    justify-content: space-between;
+    width: 70%;
+    height: 40px;
 `;
 
-const ButtonDivs = styled.div`
-  display: flex;
-  padding: 0 0;
-  width: 1100px;
+const LeftButtons = styled.div`
+    display: flex;
+    width: 420px;
+    height:34px;
+    margin-left: 32px;
 `;
 
-const Menu = styled.button`
+const RightButtons = styled.div`
+    display: flex;
+    width: 257px;
+    height:40px;
+    margin-right: 34px;
+    justify-content: end;
+    align-items: center;
+    cursor: pointer;
+`;
+
+const Menu = styled(BlackBtn)`
   width: 92px;
   height: 30px;
   margin-right: 11px;
@@ -121,10 +125,10 @@ const Menu = styled.button`
   font-size: 12px;
   font-weight: bold;
   cursor: pointer;
-  background-color: ${(props) => (props.isActive ? "#EB4646" : "#FFFFFF")};
+  background-color: ${(props) => (props.isActive ? BlackBtn : "#FFFFFF")};
   color: ${(props) => (props.isActive ? "#FFFFFF" : "#000000")};
   &:hover {
-    background-color: ${(props) => (props.isActive ? "#EB4646" : "#F4F4F4")};
+    background-color: ${(props) => (props.isActive ? BlackBtn : "#F4F4F4")};
     color: ${(props) => (props.isActive ? "#FFFFFF" : "#000000")};
   }
 `;
@@ -146,16 +150,6 @@ const CategoryIcon = styled.img`
 
 const Options = styled.div`
   display: ${(props) => (props.active ? "visible" : "none")};
-`;
-
-const WriteButtonWrapper = styled.div`
-  display: flex;
-  width: 100px;
-  height: 40px;
-  margin: 9px 0px 0px 386px;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
 `;
 
 const ItemAddButton = styled.button`
--- shoppingmall/src/pages/productPage/ItemListPage.js
+++ shoppingmall/src/pages/productPage/ItemListPage.js
@@ -2,11 +2,11 @@ import React from "react";
 import ItemList from "../../components/itemList/ItemList";
 
 
-const ProductListPage = () => {
+const ItemListPage = () => {
   return (
     <>
       <ItemList />
     </>
   );
 };
-export default ProductListPage;
+export default ItemListPage;
