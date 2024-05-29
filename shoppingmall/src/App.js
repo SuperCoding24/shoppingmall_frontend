f201cd64c914faeee9059de704b022914405482f
choimeeyoung <choimeeyoung94@gmail.com>
2024-05-29 15:09:39 +0900

    물품 목록 및 상세 조회 페이지 fetch 및 UI 수정

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -5,15 +5,16 @@ import { Route, Routes } from "react-router-dom";
 //components
 import AppLayout from "./components/commom/AppLayout";
 // pages
-import ProductListPage from "./pages/productPage/ProductListPage";
 import Login from "../src/components/userAccount/Login";
 import Signup from "./components/userAccount/Signup";
 import Cart from "./pages/cart/Cart";
 import MyPage from "./pages/myPage/MyPage";
 import Sell from "./pages/sellPage/SellPage";
 import Write from "./pages/ProductWritePage/ProductWritePage";
-import ProductDetailPage from "./pages/productPage/ProductDetailPage";
 import OrderDetailsPage from "./pages/orderDetails/OrderDetailsPage";
+import ItemListPage from "./pages/productPage/ItemListPage";
+import ItemDetailPage from "./pages/productPage/ItemDetailPage";
+
 
 function App() {
   return (
@@ -22,13 +23,13 @@ function App() {
       <AppLayout>
         <Routes>
           <Route path="/settings" element={<MyPage />} />
-          <Route path="/" element={<ProductListPage />} />
+          <Route path="/" element={<ItemListPage/>} />
           <Route path="/write" element={<Write />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/mypage" element={<MyPage />} />
-          <Route path="/product/:id" element={<ProductDetailPage />} />
+          <Route path="/product/:id" element={<ItemDetailPage/>} />
           <Route path="/sell" element={<Sell />} />
           <Route path="/order-details" element={<OrderDetailsPage />} />
         </Routes>
---
+++ shoppingmall/src/pages/productPage/ItemListPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ItemList from '../../components/itemList/ItemList';
+
+const ItemListPage = () => {
+    return (
+        <div>
+            <ItemList/>
+        </div>
+    );
+};
+
+export default ItemListPage;
+
